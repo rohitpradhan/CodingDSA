8ea8a828890bb47a30920b04165011fcdb3574c8
Rohit Pradhan <rohitpradhan60@gmail.com>
2023-01-27 21:15:22 +0530

    Longest Repeating Character Replacement

--- SlidingWindow/SlidingWindow.playground/Contents.swift
+++ SlidingWindow/SlidingWindow.playground/Contents.swift
@@ -20,3 +20,55 @@ func minSubArrayLen(_ target: Int, _ nums: [Int]) -> Int {
     }
     return minSize == Int.max ? 0 : minSize
 }
+
+
+/*
+ 424. Longest Repeating Character Replacement
+ */
+func characterReplacement(_ s: String, _ k: Int) -> Int {
+    
+    
+    var map: [Character: Int] = [:]
+    var left = 0
+    var maxlength = 0
+    var chars = Array(s)
+    
+    for right in 0..<s.count {
+        
+        let char = chars[right]
+        if let count = map[char] {
+            map[char] = count + 1
+        } else {
+            map[char] = 1
+        }
+        
+        let maxOccur = findMaxOccurance(map: map)
+        let length = right - left + 1
+        
+        if length - maxOccur <= k {
+            maxlength = max(length, maxlength)
+        } else {
+            
+            let char = chars[left]
+            
+            if let count = map[char] {
+                map[char] = count - 1
+            }
+            left += 1
+        }
+    }
+    return maxlength
+    
+}
+
+func findMaxOccurance(map: [Character: Int]) -> Int {
+    var maxOccur = 0
+    for key in map.keys {
+        if let count = map[key] {
+            maxOccur = max(maxOccur, count)
+        }
+    }
+    return maxOccur
+}
+
+characterReplacement("AABABBA", 1)
